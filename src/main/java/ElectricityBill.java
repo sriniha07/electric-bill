ff176452870f409195e205f603471c5a4ae5f7cb
sriniha07 <sriniha060@gmail.com>
2026-05-08 14:26:38 +0530

    Initial commit

--- src/main/java/ElectricityBill.java
+++ src/main/java/ElectricityBill.java
@@ -1,4 +1,15 @@
-
 public class ElectricityBill {
-
+	int unit=100;
+	public double CalculateBill(int unit) {
+		if(unit<=100) {
+			return 1.5*unit;
+		}
+		else if((unit>100)&&(unit<=300)) {
+			return 2.5*unit;
+		}
+		else {
+			return 4*unit;
+		}
+	}
+	
 }
